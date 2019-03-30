730e5720ca916d568454ea378d8d97511e2979f8
Aljona Buchloh <buchloh@synyx.de>
2019-03-30 15:09:50 +0100

    Remove date from search results due to formatting problems

--- assets/js/search.js
+++ assets/js/search.js
@@ -37,9 +37,8 @@
 
     function createPostListingHTML(postItem) {
       var headingHTML = "<h2><a  class='search-link' href='" + postItem.url + "''>" + postItem.title + "</a></h2>";
-      var metaHTML = "<div class='meta'>" + postItem.date + "</div>";
       var descriptionHTML = "<p>" + postItem.content.substring(0, 150) + "...</p>";
-      return headingHTML + metaHTML + descriptionHTML;
+      return headingHTML + descriptionHTML;
     }
 
     function displaySearchResults(results, store) {
